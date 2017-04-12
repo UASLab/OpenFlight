2f3a583af7c5fa3bb698dc6e7bf8353ce2ba9fc7
Chris Regan <rega0051@umn.edu>
2017-04-11 22:18:33 -0500

    Renamed Actuators Bus to Effectors
    Made Actuator and CntrlSurf Buses elements of a new Effectors bus. The
    actuation model can be expanded to include more generic effector models.

--- Simulation/Config/UltraStick25e/AircraftSpecificBuses_UltraStick25e.m
+++ Simulation/Config/UltraStick25e/AircraftSpecificBuses_UltraStick25e.m
@@ -1,14 +1,14 @@
 %% Initialize pilot_cmds bus
 names = {'dthr_pilot','de_pilot','dr_pilot','da_l_pilot','da_r_pilot','df_l_pilot','df_r_pilot','mode'};
 for i = 1:length(names)
-% Create BusElement for each field
-elems(i) = Simulink.BusElement;
-elems(i).Name = names{i};
-elems(i).Dimensions = 1;
-elems(i).DataType = 'double';
-elems(i).SampleTime = -1;
-elems(i).Complexity = 'real';
-elems(i).SamplingMode = 'Sample based';
+    % Create BusElement for each field
+    elems(i) = Simulink.BusElement;
+    elems(i).Name = names{i};
+    elems(i).Dimensions = 1;
+    elems(i).DataType = 'double';
+    elems(i).SampleTime = -1;
+    elems(i).Complexity = 'real';
+    elems(i).SamplingMode = 'Sample based';
 end
 BusObject = Simulink.Bus;
 BusObject.HeaderFile = '';
@@ -24,14 +24,14 @@ names = {'phi_rad','theta_rad','psi_rad','p_rps','q_rps','r_rps','Ax_mpss','Ay_m
     'GeodeticLatitude_deg','GeodeticLonggitude_deg','Altitude_m','Xdot_mps','Ydot_mps','Zdot_mps',...
     'Vs_mps','AltitudeAboveGroundLevel_m','alpha_rad','beta_rad','Pd_kPa','Ps_kPa'};
 for i = 1:length(names)
-% Create BusElement for each field
-elems(i) = Simulink.BusElement;
-elems(i).Name = names{i};
-elems(i).Dimensions = 1;
-elems(i).DataType = 'double';
-elems(i).SampleTime = -1;
-elems(i).Complexity = 'real';
-elems(i).SamplingMode = 'Sample based';
+    % Create BusElement for each field
+    elems(i) = Simulink.BusElement;
+    elems(i).Name = names{i};
+    elems(i).Dimensions = 1;
+    elems(i).DataType = 'double';
+    elems(i).SampleTime = -1;
+    elems(i).Complexity = 'real';
+    elems(i).SamplingMode = 'Sample based';
 end
 
 offset = i ;
@@ -56,14 +56,14 @@ clear elems
 %% Initialize Refs bus
 names = {'lat_cmd_guid','long_cmd_guid'};
 for i = 1:length(names)
-% Create BusElement for each field
-elems(i) = Simulink.BusElement;
-elems(i).Name = names{i};
-elems(i).Dimensions = 1;
-elems(i).DataType = 'double';
-elems(i).SampleTime = -1;
-elems(i).Complexity = 'real';
-elems(i).SamplingMode = 'Sample based';
+    % Create BusElement for each field
+    elems(i) = Simulink.BusElement;
+    elems(i).Name = names{i};
+    elems(i).Dimensions = 1;
+    elems(i).DataType = 'double';
+    elems(i).SampleTime = -1;
+    elems(i).Complexity = 'real';
+    elems(i).SamplingMode = 'Sample based';
 end
 BusObject = Simulink.Bus;
 BusObject.HeaderFile = '';
@@ -76,14 +76,14 @@ clear elems
 %% Initialize Commands bus
 names = {'throttle_nd','elevator_rad','rudder_rad','l_aileron_rad','r_aileron_rad','l_flap_rad','r_flap_rad'};
 for i = 1:length(names)
-% Create BusElement for each field
-elems(i) = Simulink.BusElement;
-elems(i).Name = names{i};
-elems(i).Dimensions = 1;
-elems(i).DataType = 'double';
-elems(i).SampleTime = -1;
-elems(i).Complexity = 'real';
-elems(i).SamplingMode = 'Sample based';
+    % Create BusElement for each field
+    elems(i) = Simulink.BusElement;
+    elems(i).Name = names{i};
+    elems(i).Dimensions = 1;
+    elems(i).DataType = 'double';
+    elems(i).SampleTime = -1;
+    elems(i).Complexity = 'real';
+    elems(i).SamplingMode = 'Sample based';
 end
 BusObject = Simulink.Bus;
 BusObject.HeaderFile = '';
@@ -95,17 +95,37 @@ clear elems
 
 
 
+%% Initialize Actuator bus
+names = {'throttle_nd', 'elevator_rad','rudder_rad','l_aileron_rad','r_aileron_rad','l_flap_rad','r_flap_rad'};
+for i = 1:length(names)
+    % Create BusElement for each field
+    elems(i) = Simulink.BusElement;
+    elems(i).Name = names{i};
+    elems(i).Dimensions = 1;
+    elems(i).DataType = 'double';
+    elems(i).SampleTime = -1;
+    elems(i).Complexity = 'real';
+    elems(i).SamplingMode = 'Sample based';
+end
+BusObject = Simulink.Bus;
+BusObject.HeaderFile = '';
+BusObject.Description = sprintf('');
+BusObject.Elements = elems;
+assignin('base', 'Actuator', BusObject);
+clear elems
+
+
 %% Initialize CtrlSurf bus
 names = {'elevator_rad','rudder_rad','l_aileron_rad','r_aileron_rad','l_flap_rad','r_flap_rad'};
 for i = 1:length(names)
-% Create BusElement for each field
-elems(i) = Simulink.BusElement;
-elems(i).Name = names{i};
-elems(i).Dimensions = 1;
-elems(i).DataType = 'double';
-elems(i).SampleTime = -1;
-elems(i).Complexity = 'real';
-elems(i).SamplingMode = 'Sample based';
+    % Create BusElement for each field
+    elems(i) = Simulink.BusElement;
+    elems(i).Name = names{i};
+    elems(i).Dimensions = 1;
+    elems(i).DataType = 'double';
+    elems(i).SampleTime = -1;
+    elems(i).Complexity = 'real';
+    elems(i).SamplingMode = 'Sample based';
 end
 BusObject = Simulink.Bus;
 BusObject.HeaderFile = '';
@@ -115,15 +135,13 @@ assignin('base', 'CtrlSurf', BusObject);
 clear elems
 
 
-%% Initialize Actuators bus
 elems(1) = Simulink.BusElement;
-elems(1).Name = 'throttle_nd';
+elems(1).Name = 'Actuator';
 elems(1).Dimensions = 1;
-elems(1).DataType = 'double';
+elems(1).DataType = 'Bus: Actuator';
 elems(1).SampleTime = -1;
 elems(1).Complexity = 'real';
 elems(1).SamplingMode = 'Sample based';
-
 
 elems(2) = Simulink.BusElement;
 elems(2).Name = 'CtrlSurf';
@@ -137,7 +155,7 @@ BusObject = Simulink.Bus;
 BusObject.HeaderFile = '';
 BusObject.Description = sprintf('');
 BusObject.Elements = elems;
-assignin('base', 'Actuators', BusObject);
+assignin('base', 'Effectors', BusObject);
 clear elems
 
 
